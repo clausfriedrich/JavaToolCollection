5489f55647edfe866b830f67b92675f04836dab0
clausfriedrich <claus.friedrich@web.de>
2015-02-07 07:14:41 +0100

    Test

--- DemoTray/src/pub/Demo/Tray/ResourceReader.java
+++ DemoTray/src/pub/Demo/Tray/ResourceReader.java
@@ -1,5 +1,5 @@
 /**
- * HIer wird was ge‰ndert
+ * HIer wird was ge√§ndert
  * 
  * 
  * 
@@ -14,6 +14,7 @@ import java.io.InputStream;
 
 import javax.swing.ImageIcon;
 
+// class
 public class ResourceReader
 {
   public Image loadImageResource(String pkgname, String fname)
